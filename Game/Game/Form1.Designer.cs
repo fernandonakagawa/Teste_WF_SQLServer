d6bbad5141d49d7aab468f875e1f7480fdf19959
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-06-15 15:35:22 -0300

    Select com DataGridView, Edição e Deleção de Registros

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -29,32 +29,45 @@ namespace Game
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btInserir = new System.Windows.Forms.Button();
             this.tbNome = new System.Windows.Forms.TextBox();
             this.ndPontos = new System.Windows.Forms.NumericUpDown();
+            this.dgvRanking = new System.Windows.Forms.DataGridView();
+            this.btAtualizar = new System.Windows.Forms.Button();
+            this.gbEditar = new System.Windows.Forms.GroupBox();
+            this.lbEditando = new System.Windows.Forms.Label();
+            this.ndPontosEditar = new System.Windows.Forms.NumericUpDown();
+            this.tbNomeEditar = new System.Windows.Forms.TextBox();
+            this.btEditar = new System.Windows.Forms.Button();
+            this.gbInserir = new System.Windows.Forms.GroupBox();
+            this.btDeletar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ndPontos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRanking)).BeginInit();
+            this.gbEditar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ndPontosEditar)).BeginInit();
+            this.gbInserir.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btInserir
             // 
-            this.button1.Location = new System.Drawing.Point(67, 168);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btInserir.Location = new System.Drawing.Point(15, 71);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 0;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.button1_Click);
             // 
             // tbNome
             // 
-            this.tbNome.Location = new System.Drawing.Point(67, 100);
+            this.tbNome.Location = new System.Drawing.Point(15, 42);
             this.tbNome.Name = "tbNome";
             this.tbNome.Size = new System.Drawing.Size(100, 23);
             this.tbNome.TabIndex = 1;
             // 
             // ndPontos
             // 
-            this.ndPontos.Location = new System.Drawing.Point(194, 100);
+            this.ndPontos.Location = new System.Drawing.Point(121, 42);
             this.ndPontos.Maximum = new decimal(new int[] {
             100000000,
             0,
@@ -64,27 +77,139 @@ namespace Game
             this.ndPontos.Size = new System.Drawing.Size(120, 23);
             this.ndPontos.TabIndex = 2;
             // 
+            // dgvRanking
+            // 
+            this.dgvRanking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRanking.Location = new System.Drawing.Point(563, 21);
+            this.dgvRanking.Name = "dgvRanking";
+            this.dgvRanking.RowTemplate.Height = 25;
+            this.dgvRanking.Size = new System.Drawing.Size(405, 446);
+            this.dgvRanking.TabIndex = 3;
+            this.dgvRanking.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRanking_CellEnter);
+            this.dgvRanking.SelectionChanged += new System.EventHandler(this.dgvRanking_SelectionChanged);
+            // 
+            // btAtualizar
+            // 
+            this.btAtualizar.Location = new System.Drawing.Point(563, 473);
+            this.btAtualizar.Name = "btAtualizar";
+            this.btAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btAtualizar.TabIndex = 4;
+            this.btAtualizar.Text = "Atualizar";
+            this.btAtualizar.UseVisualStyleBackColor = true;
+            this.btAtualizar.Click += new System.EventHandler(this.btAtualizar_Click);
+            // 
+            // gbEditar
+            // 
+            this.gbEditar.Controls.Add(this.lbEditando);
+            this.gbEditar.Controls.Add(this.ndPontosEditar);
+            this.gbEditar.Controls.Add(this.tbNomeEditar);
+            this.gbEditar.Controls.Add(this.btEditar);
+            this.gbEditar.Enabled = false;
+            this.gbEditar.Location = new System.Drawing.Point(42, 223);
+            this.gbEditar.Name = "gbEditar";
+            this.gbEditar.Size = new System.Drawing.Size(298, 211);
+            this.gbEditar.TabIndex = 5;
+            this.gbEditar.TabStop = false;
+            this.gbEditar.Text = "Editar";
+            // 
+            // lbEditando
+            // 
+            this.lbEditando.AutoSize = true;
+            this.lbEditando.Location = new System.Drawing.Point(36, 39);
+            this.lbEditando.Name = "lbEditando";
+            this.lbEditando.Size = new System.Drawing.Size(54, 15);
+            this.lbEditando.TabIndex = 6;
+            this.lbEditando.Text = "Editando";
+            // 
+            // ndPontosEditar
+            // 
+            this.ndPontosEditar.Location = new System.Drawing.Point(142, 79);
+            this.ndPontosEditar.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.ndPontosEditar.Name = "ndPontosEditar";
+            this.ndPontosEditar.Size = new System.Drawing.Size(120, 23);
+            this.ndPontosEditar.TabIndex = 5;
+            // 
+            // tbNomeEditar
+            // 
+            this.tbNomeEditar.Location = new System.Drawing.Point(36, 79);
+            this.tbNomeEditar.Name = "tbNomeEditar";
+            this.tbNomeEditar.Size = new System.Drawing.Size(100, 23);
+            this.tbNomeEditar.TabIndex = 4;
+            // 
+            // btEditar
+            // 
+            this.btEditar.Location = new System.Drawing.Point(36, 108);
+            this.btEditar.Name = "btEditar";
+            this.btEditar.Size = new System.Drawing.Size(75, 23);
+            this.btEditar.TabIndex = 3;
+            this.btEditar.Text = "Editar";
+            this.btEditar.UseVisualStyleBackColor = true;
+            this.btEditar.Click += new System.EventHandler(this.btEditar_Click);
+            // 
+            // gbInserir
+            // 
+            this.gbInserir.Controls.Add(this.btInserir);
+            this.gbInserir.Controls.Add(this.tbNome);
+            this.gbInserir.Controls.Add(this.ndPontos);
+            this.gbInserir.Location = new System.Drawing.Point(42, 101);
+            this.gbInserir.Name = "gbInserir";
+            this.gbInserir.Size = new System.Drawing.Size(288, 116);
+            this.gbInserir.TabIndex = 6;
+            this.gbInserir.TabStop = false;
+            this.gbInserir.Text = "Inserir";
+            // 
+            // btDeletar
+            // 
+            this.btDeletar.Enabled = false;
+            this.btDeletar.Location = new System.Drawing.Point(893, 473);
+            this.btDeletar.Name = "btDeletar";
+            this.btDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btDeletar.TabIndex = 7;
+            this.btDeletar.Text = "Deletar";
+            this.btDeletar.UseVisualStyleBackColor = true;
+            this.btDeletar.Click += new System.EventHandler(this.btDeletar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(652, 294);
-            this.Controls.Add(this.ndPontos);
-            this.Controls.Add(this.tbNome);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1011, 601);
+            this.Controls.Add(this.btDeletar);
+            this.Controls.Add(this.gbInserir);
+            this.Controls.Add(this.gbEditar);
+            this.Controls.Add(this.btAtualizar);
+            this.Controls.Add(this.dgvRanking);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ndPontos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRanking)).EndInit();
+            this.gbEditar.ResumeLayout(false);
+            this.gbEditar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ndPontosEditar)).EndInit();
+            this.gbInserir.ResumeLayout(false);
+            this.gbInserir.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btInserir;
         private System.Windows.Forms.TextBox tbNome;
         private System.Windows.Forms.NumericUpDown ndPontos;
+        private System.Windows.Forms.DataGridView dgvRanking;
+        private System.Windows.Forms.Button btAtualizar;
+        private System.Windows.Forms.GroupBox gbEditar;
+        private System.Windows.Forms.Label lbEditando;
+        private System.Windows.Forms.NumericUpDown ndPontosEditar;
+        private System.Windows.Forms.TextBox tbNomeEditar;
+        private System.Windows.Forms.Button btEditar;
+        private System.Windows.Forms.GroupBox gbInserir;
+        private System.Windows.Forms.Button btDeletar;
     }
 }
 
--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -22,37 +22,225 @@ namespace Game
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (tbNome.Text.Trim().Length > 0)
+            {
+                SqlConnection conexao = new SqlConnection();
+                SqlCommand sql = new SqlCommand();
+                conexao.ConnectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Game;Integrated Security=True";
+                sql.Connection = conexao;
+                /*sql.CommandText = "INSERT INTO Ranking (nome, pontos) VALUES " +
+                    "(@nome, @pontos) ";
+                sql.Parameters.AddWithValue("@nome", tbNome.Text);
+                sql.Parameters.AddWithValue("@pontos", (int)ndPontos.Value);*/
+
+                //pode ser escrito dessa forma também
+                sql.CommandText = $"INSERT INTO Ranking (nome, pontos) VALUES " +
+                    $"('{tbNome.Text.Trim()}',{(int)ndPontos.Value})";
+
+                int linhasAfetadas = 0;
+                try
+                {
+                    conexao.Open();
+                    linhasAfetadas = sql.ExecuteNonQuery();
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"A conexão com o banco falhou. Erro: {exception.ToString()}");
+                    Debug.WriteLine(exception.ToString());
+                }
+                finally
+                {
+                    conexao.Close();
+                    MessageBox.Show($"Registro feito com sucesso.\n" +
+                        $"Linhas afetadas: {linhasAfetadas} ");
+                    tbNome.Text = "";
+                    ndPontos.Value = 0;
+                    AtualizarRanking();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Preencha o nome!");
+            }
+        }
+
+        private void btAtualizar_Click(object sender, EventArgs e)
+        {
+            AtualizarRanking();
+        }
+
+        private void AtualizarRanking()
+        {
             SqlConnection conexao = new SqlConnection();
             SqlCommand sql = new SqlCommand();
             conexao.ConnectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Game;Integrated Security=True";
             sql.Connection = conexao;
-            /*sql.CommandText = "INSERT INTO Ranking (nome, pontos) VALUES " +
-                "(@nome, @pontos) ";
-            sql.Parameters.AddWithValue("@nome", tbNome.Text);
-            sql.Parameters.AddWithValue("@pontos", (int)ndPontos.Value);*/
-            
-            //pode ser escrito dessa forma também
-            sql.CommandText = $"INSERT INTO Ranking (nome, pontos) VALUES " +
-                $"('{tbNome.Text}',{(int)ndPontos.Value})";
+            sql.CommandText = $"SELECT TOP 10 * FROM Ranking " +
+                $"ORDER BY pontos DESC; ";
 
-            int linhasAfetadas = 0;
+            SqlDataAdapter adaptador = new SqlDataAdapter(sql);
+
             try
             {
                 conexao.Open();
-                linhasAfetadas = sql.ExecuteNonQuery();
+                sql.ExecuteNonQuery();
             }
-            catch(Exception exception)
+            catch (Exception ex)
             {
-                MessageBox.Show($"A conexão com o banco falhou. Erro: {exception.ToString()}");
-                Debug.WriteLine(exception.ToString());
+                MessageBox.Show($"A conexão com o banco falhou. Erro: {ex.ToString()}");
+                Debug.WriteLine(ex.ToString());
             }
             finally
             {
                 conexao.Close();
-                MessageBox.Show($"Registro feito com sucesso.\n" +
-                    $"Linhas afetadas: {linhasAfetadas} ");
+                DataTable tabela = new DataTable();
+                adaptador.Fill(tabela);
+                dgvRanking.DataSource = tabela;
+                dgvRanking.Columns["idRanking"].Visible = false;
+                /*
+                 * Explicação sobre TryParse
+                 * float x = 0;
+                if (!float.TryParse(tbNome.Text, out x)) MessageBox.Show("Impossível converter.");
+                else MessageBox.Show("Conversão feita com sucesso!");
+                */
             }
 
+        }
+
+        private void dgvRanking_SelectionChanged(object sender, EventArgs e)
+        {
+            //int celulasSelecionadas = dgvRanking.SelectedCells.Count;
+            //MessageBox.Show($"Selection Changed {celulasSelecionadas}");
+        }
+
+        private void dgvRanking_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //int celulasSelecionadas = dgvRanking.SelectedCells.Count;
+            //MessageBox.Show($"Cell Enter {celulasSelecionadas}");
+
+            if(dgvRanking.SelectedCells.Count > 0)
+            {
+                gbEditar.Enabled = true;
+                btDeletar.Enabled = true;
+                int linhaSelecionada = dgvRanking.SelectedCells[0].RowIndex;
+                if (linhaSelecionada < dgvRanking.Rows.Count - 1)
+                {
+                    string nome = dgvRanking.Rows[linhaSelecionada].Cells[1].Value.ToString();
+                    int pontos;
+                    int.TryParse(
+                        dgvRanking.Rows[linhaSelecionada].Cells[2].Value.ToString(),
+                        out pontos);
+                    tbNomeEditar.Text = nome;
+                    ndPontosEditar.Value = pontos;
+                    lbEditando.Text = $"Editando registro de {nome} pontos: {pontos}";
+                }
+                else
+                {
+                    gbEditar.Enabled = false;
+                    btDeletar.Enabled = false;
+                }
+            }
+            else
+            {
+                gbEditar.Enabled = false;
+                btDeletar.Enabled = false;
+            }
+        }
+
+        private void btEditar_Click(object sender, EventArgs e)
+        {
+            int linhaSelecionada = dgvRanking.SelectedCells[0].RowIndex;
+            int id = int.Parse(
+                dgvRanking.Rows[linhaSelecionada].Cells[0].Value.ToString());
+            string nome = tbNomeEditar.Text.Trim();
+            int pontos = (int)ndPontosEditar.Value;
+
+            if(nome.Length == 0 || pontos < 0)
+            {
+                MessageBox.Show("Erro ao editar!");
+            }
+            else
+            {
+                SqlConnection conexao = new SqlConnection();
+                SqlCommand sql = new SqlCommand();
+                conexao.ConnectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Game;Integrated Security=True";
+                sql.Connection = conexao;
+                sql.CommandText = $"UPDATE Ranking " +
+                    $"SET nome = '{nome}', pontos = {pontos} " +
+                    $"WHERE idRanking = {id}; ";
+
+                int linhasAfetadas = 0;
+                try
+                {
+                    conexao.Open();
+                    linhasAfetadas = sql.ExecuteNonQuery();
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"A conexão com o banco falhou. Erro: {exception.ToString()}");
+                    Debug.WriteLine(exception.ToString());
+                }
+                finally
+                {
+                    conexao.Close();
+                    MessageBox.Show($"Edição feita com sucesso.\n" +
+                        $"Linhas afetadas: {linhasAfetadas} ");
+                    tbNomeEditar.Text = "";
+                    ndPontosEditar.Value = 0;
+                    lbEditando.Text = "";
+                    AtualizarRanking();
+                    gbEditar.Enabled = false;
+                    btDeletar.Enabled = false;
+                }
+            }
+
+        }
+
+        private void btDeletar_Click(object sender, EventArgs e)
+        {
+            if (dgvRanking.SelectedCells.Count > 0)
+            {
+                
+                int linhaSelecionada = dgvRanking.SelectedCells[0].RowIndex;
+                int id = int.Parse(
+                    dgvRanking.Rows[linhaSelecionada].Cells[0].Value.ToString());
+                string nome = dgvRanking.Rows[linhaSelecionada].Cells[1].Value.ToString();
+
+                MessageBoxButtons botoes = MessageBoxButtons.YesNo;
+                DialogResult resposta = MessageBox.Show(
+                    $"Deseja apagar o registro de {nome}?", "Confirmação", botoes);
+                if (resposta == DialogResult.Yes)
+                {
+
+
+                    SqlConnection conexao = new SqlConnection();
+                    SqlCommand sql = new SqlCommand();
+                    conexao.ConnectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Game;Integrated Security=True";
+                    sql.Connection = conexao;
+                    sql.CommandText = $"DELETE FROM Ranking WHERE idRanking = {id};";
+
+                    int linhasAfetadas = 0;
+                    try
+                    {
+                        conexao.Open();
+                        linhasAfetadas = sql.ExecuteNonQuery();
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show($"A conexão com o banco falhou. Erro: {exception.ToString()}");
+                        Debug.WriteLine(exception.ToString());
+                    }
+                    finally
+                    {
+                        conexao.Close();
+                        MessageBox.Show($"Registro apagado com sucesso.\n" +
+                            $"Linhas afetadas: {linhasAfetadas} ");
+                        AtualizarRanking();
+                        gbEditar.Enabled = false;
+                        btDeletar.Enabled = false;
+                    }
+                }
+            }
         }
     }
 }
